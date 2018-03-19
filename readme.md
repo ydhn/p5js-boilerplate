fac0ac4449b54e7ef81c9137a4e85fb0e3857ffd
Yundo Han <han@yun.do>
2018-03-19 17:57:29 +0900

    add initial commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+# p5.js boilerplate
+
+Boilerplate for writing p5.js sketches in es6 with babel, webpack and sass.
+
+## Usage
+Cd into the directory and install the dependencies:<br />
+`npm install`
+
+Use the following command while writing a sketch to start the dev server with hot reloading:<br />
+`npm run dev`
+
+Use the following command when done to build the bundled minified result under the `/dist` folder:<br />
+`npm run build`
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+	devtool: "inline-sourcemap",
+	entry: __dirname + '/src/js/sketch.js',
+	output: {
+		path: __dirname + '/dist/',
+		publicPath: '/',
+		filename: 'js/sketch.js'
+	},
+	devServer: {
+		inline: true,
+		hot: true,
+		port: 3333
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['env']
+				}
+			},
+			{
+				test: /\.scss$/,
+				loader: "style-loader!css-loader!sass-loader"
+			}
+		]
+	},
+	plugins: [
+    new CopyWebpackPlugin([
+      { from: 'src/index.html', to: 'index.html' },
+      { from: 'src/assets', to: 'assets' }
+    ])
+  ]
+}
